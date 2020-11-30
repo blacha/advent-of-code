d2b24d7657f20ffb4b2e0c4418247600e8869866
Blayne Chard <blayne@chard.com>
2020-11-30 21:46:50 +1300

    refactor: fix eslint

---
+++ src/2020/01/index.test.ts
@@ -0,0 +1,9 @@
+import o from 'ospec';
+
+o.spec('Day1', () => {
+  o('Answer', () => {
+    console.log('');
+    console.log('Day1.Question#1', '');
+    console.log('Day1.Question#2', '');
+  });
+});
