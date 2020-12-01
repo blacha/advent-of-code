d86512c607f8ead55886a95b7366cc0a793d8102
Blayne Chard <blayne@chard.com>
2020-12-01 18:31:33 +1300

    feat: day1 2020

--- src/2020/01/index.test.ts
+++ src/2020/01/index.test.ts
@@ -1,9 +1,25 @@
 import o from 'ospec';
+import { findSum, threeFor2020, twoFor2020 } from '.';
+import { Day1Input } from './input';
 
 o.spec('Day1', () => {
+  const testValues = [1721, 979, 366, 299, 675, 1456];
+
+  o('twoFor2020', () => {
+    o(twoFor2020(testValues)).equals(514579);
+    o(findSum(testValues, 2, 2020)).equals(514579);
+    o(twoFor2020(Day1Input)).equals(1003971);
+  });
+  o('threeFor2020', () => {
+    o(threeFor2020(testValues)).equals(241861950);
+    o(threeFor2020(Day1Input)).equals(84035952);
+  });
+
   o('Answer', () => {
     console.log('');
-    console.log('Day1.Question#1', '');
-    console.log('Day1.Question#2', '');
+    console.time('2020:Day1');
+    console.log('2020:Day1.Question#1', twoFor2020(Day1Input), findSum(Day1Input, 2, 2020));
+    console.log('2020:Day1.Question#2', threeFor2020(Day1Input), findSum(Day1Input, 3, 2020));
+    console.timeEnd('2020:Day1');
   });
 });
--- src/2020/01/index.ts
+++ src/2020/01/index.ts
@@ -1,1 +1,23 @@
-console.log('ItBegins...');
+export function findSum(values: number[], numbersToUse: number, numberToFind: number, startOffset = 0): number {
+  const numberSize = values.length;
+  for (let i = startOffset; i < numberSize; i++) {
+    const value = values[i];
+    if (numbersToUse == 1) {
+      if (value == numberToFind) return numberToFind;
+      continue;
+    }
+    const nextNumber = numberToFind - value;
+    if (nextNumber < 0) continue;
+    const res = findSum(values, numbersToUse - 1, nextNumber, i + 1);
+    if (res > 0) return res * value;
+  }
+  return -1;
+}
+
+export function twoFor2020(values: number[]): number {
+  return findSum(values, 2, 2020);
+}
+
+export function threeFor2020(values: number[]): number {
+  return findSum(values, 3, 2020);
+}
