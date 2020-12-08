c7b66bfda31078ea8e406c9dd82c21dba90cd8ce
Blayne Chard <blayne@chard.com>
2020-12-08 17:49:56 +1300

    feat(2015): day 11

---
+++ packages/2015/src/11.ts
@@ -0,0 +1,100 @@
+import { AoC } from 'aocf';
+
+const alpha = 'abcdefghijklmnopqrstuvwxyz';
+const base26 = '0123456789abcdefghijklmnop';
+const toB26 = new Map<string, string>();
+const fromB26 = new Map<string, string>();
+
+for (let i = 0; i < alpha.length; i++) {
+  toB26.set(alpha[i], base26[i]);
+  fromB26.set(base26[i], alpha[i]);
+}
+
+function toNumber(str: string): number {
+  const base = [];
+  for (const ch of str) base.push(toB26.get(ch));
+  return parseInt(base.join(''), 26);
+}
+
+function fromNumber(num: number): string {
+  const alpha = [];
+  const data = num.toString(26);
+  for (const ch of data) alpha.push(fromB26.get(ch));
+  return alpha.join('');
+}
+
+// const CodeA = 'a'.charCodeAt(0);
+
+// function toNumber(str: string): number {
+//   let output = 0;
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     output += (str.charCodeAt(i) - CodeA) * Math.pow(26, str.length - i - 1);
+//   }
+//   return output;
+// }
+// function fromNumber(num: number): string {
+//   const chars = [];
+//   while (num > 0) {
+//     const mod = num % 26;
+//     num = (num / 26) | 0;
+
+//     if (mod == 0) {
+//       num--;
+//       chars.push('z');
+//     } else {
+//       chars.push(String.fromCharCode(CodeA + mod));
+//     }
+//   }
+//   return chars.reverse().join('');
+// }
+
+export class AoC2015Day11 extends AoC {
+  constructor() {
+    super(2015, 11);
+  }
+
+  isValidPassword(str: string): boolean {
+    const pairs = new Set<string>();
+    let inc = false;
+    for (let i = 0; i < str.length; i++) {
+      const chr = str[i];
+      if (chr == 'i' || chr == 'o' || chr == 'l') return false;
+      if (chr == str[i + 1]) pairs.add(chr);
+      const charCode = str.charCodeAt(i);
+      if (str[i + 1] == String.fromCharCode(charCode + 1) && str[i + 2] == String.fromCharCode(charCode + 2)) {
+        inc = true;
+      }
+    }
+    return pairs.size > 1 && inc;
+  }
+
+  findNextPassword(str: string): string {
+    if (this.ans.get(str)) return this.ans.get(str)!;
+
+    let current = toNumber(str);
+    let count = 0;
+    while (!this.isValidPassword(fromNumber(current))) {
+      count++;
+      current = current + 1;
+      if (count > 1e6) throw new Error('Failed to find');
+    }
+    const output = fromNumber(current);
+    this.ans.set(str, output);
+    return output;
+  }
+
+  ans = new Map<string, string>();
+  partA(input: string): string {
+    return this.findNextPassword(input);
+  }
+
+  partB(input: string): string {
+    const firstNumber = this.partA(input);
+    const nextNumber = fromNumber(toNumber(firstNumber) + 1);
+    return this.findNextPassword(nextNumber);
+  }
+}
+
+export const aoc2015day11 = new AoC2015Day11();
+
+aoc2015day11.test();
--- packages/2015/src/test.ts
+++ packages/2015/src/test.ts
@@ -8,6 +8,7 @@ import './07';
 import './08';
 import './09';
 import './10';
+import './11';
 
 import o from 'ospec';
 o.run();
