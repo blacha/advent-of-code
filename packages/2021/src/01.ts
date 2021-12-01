b803e67d2d334093135f7d217be202b00a2c3f8b
Blayne Chard <bchard@linz.govt.nz>
2021-12-01 22:01:23 +1300

    feat(2021): day 1

--- packages/2021/src/01.ts
+++ packages/2021/src/01.ts
@@ -10,24 +10,37 @@ export class Aoc2021Day1 extends AoC<number[]> {
   }
 
   partA(data: number[]): number {
-    return -1;
+    let count = 0;
+    let last = data[0];
+    for (let i = 1; i < data.length; i ++) {
+      let num = data[i]
+      if (last !== -0 && num > last) count++
+      last = num
+    }
+    return count;
   }
 
   partB(data: number[]): number {
-    return -1;
+    let count = 0;
+    for (let i = 0; i < data.length - 3; i++) {
+      const winA = data[i] + data[i + 1] + data[i + 2]
+      const winB = data[i + 1] + data[i + 2] + data[i + 3]
+      if (winB > winA) count++;
+    }
+    return count;
   }
 }
 
 export const aoc2021day1 = new Aoc2021Day1();
 
-// aoc2021day1.test((o) => {
-//   const testValues = [1721, 979, 366, 299, 675, 1456];
+aoc2021day1.test((o) => {
+  const testValues = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
 
-//   o('2021partA', () => {
-//     o(aoc2021day1.partA(testValues)).equals(-1);
-//   });
+  o('2021partA', () => {
+    o(aoc2021day1.partA(testValues)).equals(7);
+  });
 
-//   o('2021partB', () => {
-//     o(aoc2021day1.partB(testValues)).equals(-1);
-//   });
-// });
+  o('2021partB', () => {
+    o(aoc2021day1.partB(testValues)).equals(5);
+  });
+});
