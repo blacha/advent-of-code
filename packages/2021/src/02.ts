6196a7133a9d99ef0266e12600676e20b186fe21
Blayne Chard <bchard@linz.govt.nz>
2021-12-03 12:12:15 +1300

    feat(2021): day 2

---
+++ packages/2021/src/02.ts
@@ -0,0 +1,69 @@
+import { AoC, Point } from 'aocf';
+
+export type Direction = 'forward' | 'up' | 'down';
+interface Input {
+  direction: Direction;
+  count: number;
+}
+const aoc = AoC.create<Input[]>(2021, 2);
+
+aoc.parse = (input: string): Input[] => {
+  return input.split('\n').map((c) => {
+    const chunks = c.trim().split(' ');
+    return { direction: chunks[0] as Direction, count: Number(chunks[1]) };
+  });
+};
+
+aoc.partA = (input: Input[]): number => {
+  const pt: Point = { x: 0, y: 0 };
+  for (const i of input) {
+    switch (i.direction) {
+      case 'forward':
+        pt.x += i.count;
+        break;
+      case 'up':
+        pt.y -= i.count;
+        break;
+      case 'down':
+        pt.y += i.count;
+        break;
+      default:
+        throw new Error('Unknown direction ' + i.direction);
+    }
+  }
+  return pt.x * pt.y;
+};
+
+aoc.partB = (input: Input[]): number => {
+  const pt: Point = { x: 0, y: 0 };
+  let aim = 0;
+  for (const i of input) {
+    switch (i.direction) {
+      case 'forward':
+        pt.x += i.count;
+        pt.y += i.count * aim;
+        break;
+      case 'up':
+        aim -= i.count;
+        break;
+      case 'down':
+        aim += i.count;
+        break;
+      default:
+        throw new Error('Unknown direction ' + i.direction);
+    }
+  }
+  return pt.x * pt.y;
+};
+
+aoc.test((o) => {
+  const testValues = `forward 5\ndown 5\nforward 8\nup 3\ndown 8\nforward 2`;
+
+  o('2021partA', () => {
+    o(aoc.answers(testValues).a).equals(150);
+  });
+
+  o('2021partB', () => {
+    o(aoc.answers(testValues).b).equals(900);
+  });
+});
