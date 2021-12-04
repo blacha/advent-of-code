3c8df19d4e0d78e41d76e1b2866e36d9d753dac2
Blayne Chard <bchard@linz.govt.nz>
2021-12-04 18:45:04 +1300

    refactor: fix lint

--- packages/2021/src/04.ts
+++ packages/2021/src/04.ts
@@ -1,95 +1,95 @@
 import { AoC } from 'aocf';
 
 interface Input {
-    balls: number[]
-    ballLookup: number[]
-    boards: number[][][]
+  balls: number[];
+  ballLookup: number[];
+  boards: number[][][];
 }
 
 const aoc = AoC.create<Input>(2021, 4);
 
 aoc.parse = (input: string): Input => {
-    const lines = input.split('\n')
-    const balls = lines[0].split(',').map(Number);
+  const lines = input.split('\n');
+  const balls = lines[0].split(',').map(Number);
 
-    let ballLookup = [];
-    for (let i = 0; i < balls.length; i++) {
-        let num = balls[i];
-        ballLookup[num] = i;
+  const ballLookup = [];
+  for (let i = 0; i < balls.length; i++) {
+    const num = balls[i];
+    ballLookup[num] = i;
+  }
+
+  const boards: number[][][] = [];
+  let current: number[][] = [];
+  for (let i = 1; i < lines.length; i++) {
+    const l = lines[i].trim();
+    if (l.trim() == '') {
+      if (current.length > 0) boards.push(current);
+      current = [];
+      continue;
     }
+    current.push(l.replace(/  /g, ' ').split(' ').map(Number));
+  }
+  if (current.length > 0) boards.push(current);
+  return { boards, balls: balls, ballLookup: ballLookup };
+};
 
-    const boards: number[][][] = [];
-    let current: number[][] = []
-    for (let i = 1; i < lines.length; i++) {
-        const l = lines[i].trim();
-        if (l.trim() == '') {
-            if (current.length > 0) boards.push(current);
-            current = [];
-            continue
-        }
-        current.push(l.replace(/  /g, ' ').split(' ').map(Number))
-    }
-    if (current.length > 0) boards.push(current);
-    return { boards, balls: balls, ballLookup: ballLookup }
-}
+function boardStats(
+  lookup: number[],
+  board: number[][],
+): { board: number[][]; winX: number; winY: number; win: number } {
+  let bestXLine = Number.MAX_SAFE_INTEGER;
+  let bestYLine = Number.MAX_SAFE_INTEGER;
 
-function boardStats(lookup: number[], board: number[][]): { board: number[][], winX: number, winY: number, win: number } {
-    let bestXLine = Number.MAX_SAFE_INTEGER;
-    let bestYLine = Number.MAX_SAFE_INTEGER;
+  for (let y = 0; y < board.length; y++) {
+    const line = board[y];
 
-    for (let y = 0; y < board.length; y++) {
-        const line = board[y];
+    let maxBall = -1;
+    for (const num of line) maxBall = Math.max(maxBall, lookup[num]);
 
-        let maxBall = -1
-        for (const num of line) maxBall = Math.max(maxBall, lookup[num])
+    bestXLine = Math.min(maxBall, bestXLine);
+  }
 
-        bestXLine = Math.min(maxBall, bestXLine);
-    }
+  for (let x = 0; x < board[0].length; x++) {
+    let maxBall = -1;
 
-    for (let x = 0; x < board[0].length; x++) {
-        let maxBall = -1
+    for (const line of board) maxBall = Math.max(maxBall, lookup[line[x]]);
+    bestYLine = Math.min(maxBall, bestYLine);
+  }
 
-        for (const line of board)  maxBall = Math.max(maxBall, lookup[line[x]])
-        bestYLine = Math.min(maxBall, bestYLine);
-    }
-
-    return { board, winX: bestXLine, winY: bestYLine, win: Math.min(bestXLine, bestYLine) }
+  return { board, winX: bestXLine, winY: bestYLine, win: Math.min(bestXLine, bestYLine) };
 }
 
 function sumBoard(board: number[][], lookup: number[], since: number): number {
-    let sum = 0;
-    for (let line of board) {
-        for (const num of line) {
-            const lineIndex = lookup[num];
-            if (lineIndex <= since) continue;
-            sum += num
-        }
+  let sum = 0;
+  for (const line of board) {
+    for (const num of line) {
+      const lineIndex = lookup[num];
+      if (lineIndex <= since) continue;
+      sum += num;
     }
-    return sum;
+  }
+  return sum;
 }
-
 
 aoc.partA = (input: Input): number => {
-    const stats = input.boards.map(c => boardStats(input.ballLookup, c))
+  const stats = input.boards.map((c) => boardStats(input.ballLookup, c));
 
-    let best = stats[0];
-    for (const stat of stats) if (stat.win < best.win) best=stat;
-    const sum = sumBoard(best.board, input.ballLookup, best.win);
-    const num = input.balls[best.win]
-    return sum * num
-}
-
+  let best = stats[0];
+  for (const stat of stats) if (stat.win < best.win) best = stat;
+  const sum = sumBoard(best.board, input.ballLookup, best.win);
+  const num = input.balls[best.win];
+  return sum * num;
+};
 
 aoc.partB = (input: Input): number => {
-    const stats = input.boards.map(c => boardStats(input.ballLookup, c))
+  const stats = input.boards.map((c) => boardStats(input.ballLookup, c));
 
-    let best = stats[0];
-    for (const stat of stats) if (stat.win > best.win) best=stat;
-    const sum = sumBoard(best.board, input.ballLookup, best.win);
-    const num = input.balls[best.win]
-    return sum * num
-}
-
+  let best = stats[0];
+  for (const stat of stats) if (stat.win > best.win) best = stat;
+  const sum = sumBoard(best.board, input.ballLookup, best.win);
+  const num = input.balls[best.win];
+  return sum * num;
+};
 
 const testValues = `
 7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
@@ -110,14 +110,14 @@ const testValues = `
 10 16 15  9 19
 18  8 23 26 20
 22 11 13  6  5
- 2  0 12  3  7`.trim()
+ 2  0 12  3  7`.trim();
 
 aoc.test((o) => {
-    o('2021partA', () => {
-        o(aoc.answers(testValues).a).equals(4512);
-    });
+  o('2021partA', () => {
+    o(aoc.answers(testValues).a).equals(4512);
+  });
 
-    o('2021partB', () => {
-        o(aoc.answers(testValues).b).equals(1924);
-    });
+  o('2021partB', () => {
+    o(aoc.answers(testValues).b).equals(1924);
+  });
 });
