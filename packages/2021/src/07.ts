3ddc4626880b1f9de92e3db8d1fb219aa16e3ec7
Blayne Chard <blayne@chard.com>
2021-12-07 23:54:27 +1300

    perf(2021): optimise part 1 of day 7

--- packages/2021/src/07.ts
+++ packages/2021/src/07.ts
@@ -6,19 +6,18 @@ export type Input = number[];
 const aoc = AoC.create<Input>(2021, 7);
 
 aoc.parse = (l: string): Input => {
-  return l.split(',').map(Number);
+  return l
+    .split(',')
+    .map(Number)
+    .sort((a, b) => a - b);
 };
 
 aoc.partA = (input: Input): number => {
-  const stats = Iter.stats(input);
-
-  let minOut = Number.MAX_VALUE;
-  for (let i = stats.min.v; i < stats.max.v; i++) {
-    let sum = 0;
-    for (const j of input) sum += Math.abs(j - i);
-    if (sum < minOut) minOut = sum;
-  }
-  return minOut;
+  // Since it takes a equal number of moves to move
+  const bestIndex = input[Math.floor(input.length / 2)];
+  let sum = 0;
+  for (const i of input) sum += Math.abs(i - bestIndex);
+  return sum;
 };
 
 aoc.partB = (input: Input): number => {
