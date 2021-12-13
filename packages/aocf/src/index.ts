e7c858935abe6285da9bd48a4587599a2e23a14b
Blayne Chard <blayne@chard.com>
2021-12-13 19:31:21 +1300

    feat(aocf): add ocr for ascii art

--- packages/aocf/src/index.ts
+++ packages/aocf/src/index.ts
@@ -3,6 +3,7 @@ export { toNumberArray, toNumberArraySorted } from './util/parse';
 export { permutations } from './util/combonitorics';
 export { Iter } from './util/iter';
 export { Grid } from './util/grid';
+export { GridCharacter } from './util/grid.character';
 export { timer } from './util/timer';
 export { log } from './util/log';
 export { AoCDataFile, AoCAnswer, AoCJson } from './export';
