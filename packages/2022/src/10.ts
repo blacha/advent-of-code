ce644c9c48016291fb5e86f25dbbe244cbcf6033
Blayne Chard <blayne@chard.com>
2022-12-12 20:50:05 +1300

    feat(2022): day 10 & 11

---
+++ packages/2022/src/10.ts
@@ -0,0 +1,63 @@
+import { AoC } from 'aocf';
+import { AoCOcr } from 'aoc-ocr';
+
+export type Actions = { action: 'noop' } | { action: 'addx'; count: number };
+export type Input = Actions[];
+
+const aoc = AoC.create<Input>(2022, 10);
+
+aoc.parse = (l: string): Input => {
+  return l
+    .trim()
+    .split('\n')
+    .map((c) => {
+      if (c.startsWith('noop')) return { action: 'noop' };
+      if (c.startsWith('addx')) return { action: 'addx', count: Number(c.slice('addx'.length + 1)) };
+      throw new Error('Unknown');
+    });
+};
+
+function runProgram(input: Input): { total: number; screen: string[] } {
+  let total = 0;
+
+  const screen: string[] = [];
+
+  function onCycle(): void {
+    const cyclePer40 = cycle % 40;
+    const offset = Math.abs(cyclePer40 - x);
+    if (offset <= 1) screen[cycle] = '⬜';
+    else screen[cycle] = '⬛';
+    cycle++;
+
+    if (cycle == 20 || (cycle - 20) % 40 == 0) total += cycle * x;
+  }
+  let x = 1;
+
+  let cycle = 0;
+  for (let i = 0; i < input.length; i++) {
+    const inp = input[i];
+    if (inp.action == 'noop') {
+      onCycle();
+    } else if (inp.action == 'addx') {
+      onCycle();
+      onCycle();
+      x += inp.count;
+      continue;
+    }
+  }
+  return { screen, total };
+}
+
+aoc.partA = (input: Input): number => runProgram(input).total;
+aoc.partB = (input: Input): string => {
+  const ret = runProgram(input);
+  const crt: string[][] = [];
+  for (let i = 0; i < 6; i++) {
+    crt.push(ret.screen.slice(i * 40, (i + 1) * 40 - 1));
+  }
+  return AoCOcr.parseAll(crt, '⬜');
+};
+
+aoc.test();
+
+aoc.fetch(true);
---
+++ packages/2022/src/11.ts
@@ -0,0 +1,98 @@
+import { AoC } from 'aocf';
+import { time } from 'console';
+
+export type Input = Monkey[];
+
+export interface Monkey {
+  id: number;
+  items: number[];
+  operation: [string, string | number];
+  test: number;
+  ifTrue: number;
+  ifFalse: number;
+  count: number;
+}
+
+const aoc = AoC.create<string>(2022, 11);
+
+function getMonkies(l: string): Monkey[] {
+  const monks: Input = [];
+  const chunks = l.split('\n\n');
+  for (const m of chunks) {
+    const lines = m.split('\n');
+    const items = lines[1].split(':')[1].split(', ').map(Number);
+    const operation: [string, string | number] = lines[2].split('Operation: new = old ')[1].split(' ') as any;
+    if (operation[1] != 'old') operation[1] = Number(operation[1]);
+    const test = Number(lines[3].split('Test: divisible by')[1]);
+    const ifTrue = Number(lines[4].split('  If true: throw to monkey ')[1]);
+    const ifFalse = Number(lines[5].split('  If false: throw to monkey ')[1]);
+    const monkey = { id: monks.length, test, items, ifTrue, ifFalse, operation, count: 0 };
+    monks.push(monkey as Monkey);
+  }
+
+  return monks;
+}
+
+function getVal(item: number, monkey: Monkey): number {
+  if (monkey.operation[1] === 'old') return item;
+  if (typeof monkey.operation[1] === 'number') return monkey.operation[1];
+  throw new Error('Unknown op:' + JSON.stringify(monkey));
+}
+function runOp(item: number, monkey: Monkey): number {
+  const val = getVal(item, monkey);
+  const op = monkey.operation[0];
+
+  if (op == '+') return val + item;
+  else if (op == '*') return val * item;
+  else throw new Error('Unk');
+}
+
+aoc.partA = (input: string): number => {
+  const monkies = getMonkies(input);
+  for (let i = 0; i < 20; i++) {
+    for (const monkey of monkies) {
+      while (monkey.items.length > 0) {
+        monkey.count++;
+        const item = monkey.items.shift();
+        if (item == null) throw new Error('NoItem');
+        const ret = Math.floor(runOp(item, monkey) / 3);
+        if (ret % monkey.test == 0) {
+          monkies[monkey.ifTrue].items.push(ret);
+        } else {
+          monkies[monkey.ifFalse].items.push(ret);
+        }
+      }
+    }
+  }
+  monkies.sort((a, b) => b.count - a.count);
+  const maxA = monkies[0].count;
+  const maxB = monkies[1].count;
+  return maxA * maxB;
+};
+aoc.partB = (l: string): number => {
+  const monkies = getMonkies(l);
+
+  let prod = 1;
+  for (const mnk of monkies) prod *= mnk.test;
+  for (let i = 0; i < 10000; i++) {
+    for (const monkey of monkies) {
+      while (monkey.items.length > 0) {
+        monkey.count++;
+        const item = monkey.items.shift();
+        if (item == null) throw new Error('NoItem');
+        const ret = runOp(item, monkey) % prod;
+        if (ret % monkey.test == 0) {
+          monkies[monkey.ifTrue].items.push(ret);
+        } else {
+          monkies[monkey.ifFalse].items.push(ret);
+        }
+      }
+    }
+  }
+  monkies.sort((a, b) => b.count - a.count);
+  const maxA = monkies[0].count;
+  const maxB = monkies[1].count;
+  return maxA * maxB;
+};
+
+aoc.test();
